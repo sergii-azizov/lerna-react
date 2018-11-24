a111769bc2dafc356f2d7a7a81c49146dbdd7c61
serhii.azizov <serhii.azizov@continuum.net>
2018-11-24 15:42:15 +0200

    change readme

--- packages/portal/src/toggle-components-mounting/index.js
+++ packages/portal/src/toggle-components-mounting/index.js
@@ -14,6 +14,7 @@ export default class ToggleComponentsMounting extends Component {
             Icon2: lazyLoad('icon'),
             Icon3: lazyLoad('icon')
         });
+
         setTimeout(this.removedIcons, 2000);
     };
 
