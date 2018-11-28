a0e4c47a6fe3bcd0c11d49573e6eed8686a83171
serhii.azizov <serhii.azizov@continuum.net>
2018-11-28 17:17:42 +0200

    redux

--- packages/core/src/load-module/constants.js
+++ packages/core/src/load-module/constants.js
@@ -1,5 +1,5 @@
 const STATIC_SERVERS = {
-    DT: 'https://rawcdn.githack.com/sergii-azizov/lerna-react/174c26cda9565a25e16df50d7bbc0e4f59a9c45a/dist'
+    DT: 'https://rawcdn.githack.com/sergii-azizov/lerna-react/167ab284f41228de9936b30d2352a8a20e5a6980/dist'
 };
 
 export const STATIC_SERVER = STATIC_SERVERS.DT;
