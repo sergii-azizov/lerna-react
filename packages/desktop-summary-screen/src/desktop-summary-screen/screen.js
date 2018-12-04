0f06e293c495bb9d444643e74cf3945c904bfb8b
serhii.azizov <serhii.azizov@continuum.net>
2018-12-04 13:54:05 +0200

    preparing to presentation

--- packages/desktop-summary-screen/src/desktop-summary-screen/screen.js
+++ packages/desktop-summary-screen/src/desktop-summary-screen/screen.js
@@ -2,16 +2,14 @@ import { PureComponent } from 'react';
 import { loadModule, MS_NAMES } from 'core';
 import styles from './screen.scss';
 
-const TaskSelector = loadModule(MS_NAMES.TASK_SELECTOR);
-const Button = loadModule(MS_NAMES.COMPONENTS, { componentName: 'Button' });
+const Counter = loadModule(MS_NAMES.COMPONENTS);
 
 class DesktopSummaryScreen extends PureComponent {
     render() {
       return (
           <div className={styles.root}>
               <h1>Desktop Summary</h1>
-              <TaskSelector />
-              <Button>desktop summary button</Button>
+              <Counter />
           </div>
       );
     }
